9ca05184fcf1f155fd84516c24420d8c0b876c27
Arun Ramamurthy <contact@arun.run>
2018-11-22 11:01:00 -0800

    Minor code review

--- R/concept.R
+++ R/concept.R
@@ -1,10 +1,10 @@
 ###################
 #### STABILITY ####
-ES.lm <- function(.data, formula, V = 100, perturb_fn = perturb) { ## perturb can be user-defined specially
+ES.lm <- function(.data, lm_formula, V = 100, perturb_fn = perturb) {
   simulate_single_yhat <- function() {
     .data %>%
       perturb_fn() %>%
-      lm(formula = formula) %>%
+      lm(formula = lm_formula) %>%
       predict()
   }
 
@@ -14,69 +14,74 @@ ES.lm <- function(.data, formula, V = 100, perturb_fn = perturb) { ## perturb ca
     V_yhats %>%
     purrr::map_dbl(~ norm(. - mean_yhat, type = "2")) %>%
     mean()
-  ES <-
-    T_hat / norm(mean_yhat, type = "2")
+  ES <- T_hat / norm(mean_yhat, type = "2")
   ES
 }
 ###################
 
 #########################
 #### DATA GENERATION ####
-generate_uniform_covariate <- function(n = 150, bound = 10) {
+generate_uniform_vector <- function(n = 150, bound = 10) {
   runif(n, -bound, bound)
 }
 
-generate_normal_covariate <- function(n = 150, sd = 5) {
+generate_normal_vector <- function(n = 150, sd = 5) {
   rnorm(n, sd = sd)
 }
 
-generate_data <- function(n = 150, k = 3, generation_fn = generate_uniform_covariate, formula = x1 + x2 + x3) {
-  formula <- rlang::enquo(formula)
+#' @import magrittr
+generate_data <- function(n = 150, k = 3,
+                          covariate_generation_fn = generate_uniform_vector,
+                          y_formula, y_noise_fn = purrr::partial(generate_normal_vector, sd = 1)) {
+  y_formula <- rlang::enquo(y_formula)
   covariates <-
-    purrr::rerun(k, purrr::partial(generation_fn, n = n)()) %>%
+    purrr::rerun(k, purrr::partial(covariate_generation_fn, n = n)()) %>%
     dplyr::bind_cols()
   covariates %>%
-    dplyr::rename_all(dplyr::funs(stringr::`str_sub<-`(string = ., value = "x", start = 1, end = 1))) %>%
-    dplyr::mutate(y := !! formula)
+    dplyr::rename_all(dplyr::funs(stringr::`str_sub<-`(string = ., value = "x_", start = 1, end = 1))) %>%
+    dplyr::mutate(y := (!! y_formula) + purrr::partial(y_noise_fn, n = n)())
 }
 
-add_collinearity <- function(.data, formula, random) {
-  formula <- rlang::enquo(formula)
+augment_collinearity <- function(.data,
+                                 new_col_formula, new_col_noise_fn = purrr::partial(generate_normal_vector, sd = 1)) {
+  new_col_formula <- rlang::enquo(new_col_formula)
   n <- nrow(.data)
-  integer = sample(100, size = 1)
+  integer <- sample(100, size = 1)
   while ((paste('x_', integer, sep = '')) %in% names(.data)){
-    integer = sample(100, size = 1)
+    integer <- sample(100, size = 1)
   }
-  .data %>% mutate((!!paste('x_', integer, sep = '')) := (!!formula) + purrr::partial(random, n = n)())
+  .data %>%
+    dplyr::mutate((!! paste('x_', integer, sep = '')) :=
+                    (!! new_col_formula) + purrr::partial(new_col_noise_fn, n = n)())
 }
 
-add_multicoll <- function(.data, formula, random, k) {
-  formula <- rlang::enquo(formula)
-  add_coll <- purrr::partial(add_collinearity, formula = (!!formula), random = random)
-  multicoll_fxn <- purrr::rerun(k, add_coll) %>% purrr::reduce(purrr::compose)
-  multicoll_fxn(.data)
+augment_multicollinearity <- function(.data, k = 3,
+                                      new_col_formula, new_col_noise_fn = purrr::partial(generate_normal_vector, sd = 1)) {
+  new_col_formula <- rlang::enquo(new_col_formula)
+  augment_col <- purrr::partial(augment_collinearity,
+                          new_col_formula = (!! new_col_formula),
+                          new_col_noise_fn = new_col_noise_fn)
+  augment_multicol <- purrr::rerun(k, augment_col) %>% purrr::reduce(purrr::compose)
+  augment_multicol(.data)
 }
 #########################
 
 ######################
 #### PERTURBATION ####
-perturb <- function(.data, sigma = 1, variable = x) {
-  variable <- enquo(variable)
-  raw_vector = .data %>% pull(!! variable)
-  additional_noise <- rnorm(n = length(raw_vector), sd = sigma)
+perturb <- function(.data, variable = x, sd = 1) {
+  variable <- rlang::enquo(variable)
+  raw_vector <- .data %>% dplyr::pull(!! variable)
+  additional_noise <- rnorm(n = length(raw_vector), sd = sd)
   .data %>%
-    mutate(perturbed := (!! variable) + additional_noise) %>%
-    select_if(is.numeric) %>%
-    select(- (!! variable))
+    dplyr::mutate((!! variable) := (!! variable) + additional_noise)
 }
 
-perturb_unif <- function(.data, sigma = 1, variable = x) {
-  variable <- enquo(variable)
-  raw_vector = .data %>% pull(!! variable)
-  additional_noise <- runif(n = length(raw_vector), min = -sigma, max = sigma)
+perturb_unif <- function(.data, variable = x, bound = 1) {
+  variable <- rlang::enquo(variable)
+  raw_vector = .data %>% dplyr::pull(!! variable)
+  additional_noise <- runif(n = length(raw_vector), min = -bound, max = bound)
   .data %>%
-    mutate(additional_noise,
-           perturbed := (!! variable) + additional_noise)
+    dplyr::mutate((!! variable) := (!! variable) + additional_noise)
 }
 
 estimate_perturbed_slope <- function(.data, coeff = c("perturbed", "(Intercept)"), perturb_fn = perturb) {
@@ -84,22 +89,6 @@ estimate_perturbed_slope <- function(.data, coeff = c("perturbed", "(Intercept)"
   .data %>%
     perturb_fn() %>%
     lm(formula = y ~ perturbed) %>%
-    use_series(coefficients) %>% extract(coeff)
-}
-
-<<<<<<< HEAD
-add_collinearity <- function(.data) {
-  #create more x columns that are just +c of each other
-  .data['x_1'] = .data$x+1
-  .data['x_2'] = .data$x+2
-  .data['x_3'] = .data$x+3
-  .data
+    magrittr::use_series(coefficients) %>% magrittr::extract(coeff)
 }
 ######################
-=======
-######################
-
-
-
-
->>>>>>> c101eeb11261efd832a1a7d0b082b3c4ea7d4e8c
